0ee40aafad0428801254a4c10a39aee05132c655
Tejas Dudhal <116336036+TejasD36@users.noreply.github.com>
2024-07-19 15:56:34 +0530

    completed configurations and executed successfully

---
+++ lib/razorpay_screen.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:razorpay_flutter/razorpay_flutter.dart';
+
+class RazorpayScreen extends StatefulWidget {
+  const RazorpayScreen({super.key});
+
+  @override
+  State<RazorpayScreen> createState() => _RazorpayScreenState();
+}
+
+class _RazorpayScreenState extends State<RazorpayScreen> {
+  late Razorpay _razorpay;
+
+  TextEditingController amtController = TextEditingController();
+
+  void openCheckout(amount) async {
+    amount = amount * 100;
+    var options = {
+      'key': 'rzp_test_1DP5mmOlF5G5ag',
+      'amount': amount,
+      'name': 'King TP',
+      'prefill': {
+        'contact': '1234567890',
+        'email': 'test@gmail.com',
+      },
+      'external': {
+        'wallets': ['paytm']
+      }
+    };
+    try {
+      _razorpay.open(options);
+    } catch (e) {
+      debugPrint('Error :$e');
+    }
+  }
+
+  void handlePaymentSuccess(PaymentSuccessResponse response) {
+    Fluttertoast.showToast(
+        msg: "Payment Successful:  ${response.paymentId!}",
+        toastLength: Toast.LENGTH_SHORT);
+  }
+
+  void handlePaymentError(PaymentFailureResponse response) {
+    Fluttertoast.showToast(
+        msg: "Payment Fail:  ${response.message!}",
+        toastLength: Toast.LENGTH_SHORT);
+  }
+
+  void handleExternalWallet(ExternalWalletResponse response) {
+    Fluttertoast.showToast(
+        msg: "External Wallet:  ${response.walletName!}",
+        toastLength: Toast.LENGTH_SHORT);
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _razorpay.clear();
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    _razorpay = Razorpay();
+    _razorpay.on(Razorpay.EVENT_PAYMENT_SUCCESS, handlePaymentSuccess);
+    _razorpay.on(Razorpay.EVENT_PAYMENT_ERROR, handlePaymentError);
+    _razorpay.on(Razorpay.EVENT_EXTERNAL_WALLET, handleExternalWallet);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[800],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.1,
+            ),
+            Image.network(
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQj5NIBek3gFKtIxx-wk0Ruf3Q4Z8XAgctk1pyi3EFanuuNUjVRVhTWPNduILwQ2tvZfls&usqp=CAU',
+              height: MediaQuery.of(context).size.height * 0.08,
+              width: MediaQuery.of(context).size.width * 0.75,
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.03,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: TextFormField(
+                controller: amtController,
+                cursorColor: Colors.white,
+                autofocus: false,
+                style: const TextStyle(color: Colors.white),
+                decoration: const InputDecoration(
+                  labelText: 'Enter Amount to be Paid',
+                  labelStyle: TextStyle(fontSize: 15, color: Colors.white),
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                    color: Colors.white,
+                    width: 1,
+                  )),
+                  errorStyle: TextStyle(color: Colors.redAccent, fontSize: 15),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please Enter Amount to be Paid';
+                  }
+                  return null;
+                },
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.04,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                if (amtController.text.toString().isNotEmpty) {
+                  setState(() {
+                    int amount = int.parse(amtController.text.toString());
+                    openCheckout(amount);
+                  });
+                }
+              },
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
+              child: const Padding(
+                padding: EdgeInsets.all(8),
+                child: Text('Make Payment'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
